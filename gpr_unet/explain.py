92271ec3381f6601dc08d9e6b4b0ffcc0701034e
marcos <marcos.gomesjacinto@gmail.com>
2022-05-02 18:31:00 -0300

    first experiments

---
+++ gpr_unet/explain.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import mlflow
+import numpy as np
+import shap
+import tensorflow as tf
+
+shap.explainers._deep.deep_tf.op_handlers[
+    "Conv2DBackpropInput"
+] = shap.explainers._deep.deep_tf.passthrough
+tf.compat.v1.disable_v2_behavior()
+
+
+def get_explainer(run_id: str, background_samples: np.ndarray) -> shap.DeepExplainer:
+
+    run = mlflow.get_run(run_id)
+    artifact_uri = Path(run.info.artifact_uri)
+    model_path = artifact_uri / "model/data/model"
+
+    model = tf.keras.models.load_model(model_path)
+    flattened_model = tf.keras.models.Sequential()
+    flattened_model.add(model)
+    flattened_model.add(tf.keras.layers.Flatten())
+
+    return shap.DeepExplainer(flattened_model, background_samples)
